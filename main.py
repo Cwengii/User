9c0b3370bcd6834001131b8e1ef2dfa393449336
Acwengile Morris <acwengilebongi@gmail.com>
2024-12-06 12:08:30 +0200

    Final Commit

---
+++ main.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+surname = input("Enter your surname: ")
+age = input("Enter your age: ")
+
+print(f"Hi i am {name} {surname}, {age} years old")
+
+#Personalised greeting to user through the input
